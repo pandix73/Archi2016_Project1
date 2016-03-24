353311feb43e65464d270b7a2b83718552b1b41c
pandix73 <conan11conan112004@gmail.com>
2016-03-24 22:38:15 +0800

    30%complete

--- studentID_103062135/simulator/readbin.c
+++ studentID_103062135/simulator/readbin.c
@@ -52,17 +52,16 @@ unsigned int reg[32];
 
 void read_i_memory(int load_num){
 	int i, opcode;
+	int funct, rs, rt, rd, shamt, C;
 	for(i = 0; i < load_num; i++){
 		opcode = i_memory[i] >> 26;
 		switch(opcode){
-
 			case R: 
-				printf("R\n");
-				int funct = i_memory[i] << 26 >> 26;
-				int rs = i_memory[i] << 6 >> 26;
-				int rt = i_memory[i] << 11 >> 26;
-				int rd = i_memory[i] << 16 >> 26;
-				int shamt = i_memory[i] << 21 >> 26;
+				funct = i_memory[i] << 26 >> 26;
+				rs = i_memory[i] << 6 >> 27;
+				rt = i_memory[i] << 11 >> 27;
+				rd = i_memory[i] << 16 >> 27;
+				shamt = i_memory[i] << 21 >> 27;
 				switch(funct){
 					case add:
 						printf("add\n");
@@ -113,23 +112,98 @@ void read_i_memory(int load_num){
 						reg[rd] = reg[rt] >> shamt;
 						break;
 					case jr:
+						//PC_now = 4*i + PC
+						//PC_now = reg[rs]
 						printf("jr\n");
-						PC = reg[rs];
+						i = reg[rs] >> 2 - PC >> 2 - 1;
 						break;
 				}
 				break;
 
 			case j:
+				//PC_now = (PC_now+4)>>28<<28 | C<<2
+				//PC_now = 4*i + PC
+				printf("j\n");
+				C = i_memory[i] << 6 >> 6;
+				i = (((4 * i + PC + 4) >> 28 << 28 | C << 2) - PC) >> 2 - 1;
 				break;
-
 			case jal:
+				//PC_now = 4*i + PC
+				printf("jal\n");
+				C = i_memory[i] << 6 >> 6;
+				reg[31] = (PC + 4 * i) + 4;
 				break;
-
 			case halt: 
 				break;
-
 			default:
-				break;
+				rs = i_memory[i] << 6 >> 27;
+				rt = i_memory[i] << 11 >> 27;
+				C = i_memory[i] << 16 >> 16;
+				switch(opcode){
+					case addi:
+						printf("addi\n");
+						reg[rt] = reg[rs] + C;
+						break;
+					case addiu:
+						printf("addiu\n");
+						reg[rt] = reg[rs] + C;
+						break;
+					case lw:
+						printf("lw\n");
+						break;
+					case lh:
+						printf("lh\n");
+						//reg[rd] = reg[rs] & reg[rt];
+						break;
+					case lhu:
+						printf("lhu\n");
+						//reg[rd] = reg[rs] | reg[rt];
+						break;
+					case sw:
+						printf("sw\n");
+						//reg[rd] = reg[rs] ^ reg[rt];
+						break;
+					case sh:
+						printf("sh\n");
+						//reg[rd] = ~(reg[rs] | reg[rt]);
+						break;
+					case sb:
+						printf("sb\n");
+						//reg[rd] = ~(reg[rs] & reg[rt]);
+						break;
+					case lui:
+						printf("lui\n");
+						//reg[rd] = (reg[rs] < reg[rt]);
+						break;
+					case andi:
+						printf("andi\n");
+						//reg[rd] = reg[rt] << shamt;
+						break;
+					case ori:
+						printf("ori\n");
+						//reg[rd] = reg[rt] >> shamt;
+						break;
+					case nori:
+						printf("nori\n");
+						//reg[rd] = reg[rt] >> shamt;
+						break;
+					case slti:
+						printf("slti\n");
+						//PC = reg[rs];
+						break;
+					case beq:
+						printf("beq\n");
+						break;
+					case bne:
+						printf("bne\n");
+						break;
+					case bgtz:
+						printf("bgtz\n");
+						break;
+					default:
+						break;
+				}
+			break;
 		}
 	}
 }
@@ -181,16 +255,18 @@ int main () {
 	if (i_buffer == NULL || d_buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
 
 	// copy the file into the buffer:
-	i_result = fread(i_buffer, 1, i_size, i_file);
-	d_result = fread(d_buffer, 1, d_size, d_file);
+	i_result = fread(i_memory, 1, i_size, i_file);
+	d_result = fread(d_memory, 1, d_size, d_file);
 
 	if (i_result != i_size || d_result != d_size) {fputs ("Reading error",stderr); exit (3);}
 
 	/* the whole file is now loaded in the memory buffer. */
 
-	make_i_memory(i_buffer);
-	make_d_memory(d_buffer);
-
+	//make_i_memory(i_buffer);
+	//make_d_memory(d_buffer);
+	
+	read_i_memory(i_size);
+	
 	// terminate
 	fclose(i_file);
 	fclose(d_file);
